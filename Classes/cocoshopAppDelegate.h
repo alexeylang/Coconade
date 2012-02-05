06fe0de7ced253a1e2e0ceaf0cb5458084a8d7a8
Alexey Lang <alekseylang@gmail.com>
2012-02-05 16:39:59 +0400

    cocshopAppDelegate: remove IBOutlet attribute from properties window and glView - we will create them programmatically.

--- Classes/cocoshopAppDelegate.h
+++ Classes/cocoshopAppDelegate.h
@@ -38,8 +38,8 @@
 	NSString *filenameToOpen_;
 }
 
-@property (assign) IBOutlet NSWindow	*window;
-@property (assign) IBOutlet CSMacGLView	*glView;
+@property (assign) NSWindow *window;
+@property (assign) CSMacGLView *glView;
 @property (assign) IBOutlet CSObjectController *controller;
 @property (readwrite, copy) NSString *filenameToOpen;
 @property (readwrite) BOOL appIsRunning;
