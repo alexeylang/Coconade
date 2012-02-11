ca5be4c49986e5563b8296be81dbd6646ac4b19f
Stepan Generalov <psi.pk.ru@gmail.com>
2012-02-11 19:21:05 +0400

    CCNScene: implement borders, targetNode & background.

--- Coconade/Classes/CCNScene.h
+++ Coconade/Classes/CCNScene.h
@@ -9,8 +9,36 @@
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
 
-@interface CCNScene : CCScene {
+/** @class CCNScene Scene subclass that is used as running scene in Coconade.
+ * CCNScene incapsulates Cocos2D nodes that are used as a part of Coconade UI
+ * (checkerboard background, scene borders, etc).
+ * It holds current selected root node, providing parent for non-CCScene
+ * root nodes.
+ */
+@interface CCNScene : CCScene 
+{
+    CCNode *_targetNode;
+    CCSprite *_backgroundSprite;
     
+    BOOL _showBorders;
 }
+
+/**
+ * When set to YES - CCNScene renders border around itself.
+ */
+@property(readwrite) BOOL showBorders;
+
+/** Current selected root node of Coconade, that we work in.
+ * It can be CCScene - then this class will delegate visit to targetNode.
+ * Can be nil.
+ */
+@property(readwrite, retain) CCNode *targetNode;
+
+/** Repeated sprite that is used as a background. */
+@property(readwrite, retain) CCSprite *backgroundSprite;
+
+
+/** Updates it's size & contents to fit current winSize. */
+- (void) updateForScreenReshape;
 
 @end
--- Coconade/Classes/CCNScene.m
+++ Coconade/Classes/CCNScene.m
@@ -11,4 +11,121 @@
 
 @implementation CCNScene
 
+@synthesize showBorders = _showBorders;
+@synthesize targetNode = _targetNode;
+
+@dynamic backgroundSprite;
+
+- (CCSprite *) backgroundSprite
+{
+    return [[_backgroundSprite retain] autorelease];
+}
+
+- (void) setBackgroundSprite:(CCSprite *)backgroundSprite
+{
+    if (backgroundSprite != _backgroundSprite)
+    {
+        CCSprite *oldValue = _backgroundSprite;
+        _backgroundSprite = [backgroundSprite retain];
+        
+        ccTexParams params = {GL_LINEAR,GL_LINEAR,GL_REPEAT,GL_REPEAT};
+		[_backgroundSprite.texture setTexParameters:&params];
+		_backgroundSprite.position = ccp(0,0);
+        _backgroundSprite.anchorPoint = ccp(0,0);
+        
+        [oldValue release];
+    }
+}
+
+#pragma mark No AMC Support
+
++ (BOOL) AMCEnabled
+{
+    return NO;
+}
+
+- (NSDictionary *) dictionaryRepresentation
+{
+    return nil;
+}
+
+- (id) initWithDictionaryRepresentation:(NSDictionary *)aDict
+{
+    NSLog(@"CCNScene doesn't support AMC!");
+    
+    [self release];
+    return nil;
+}
+
+#pragma mark Init / DeInit
+
+- (id) init
+{
+    self = [super init];
+    if (self)
+    {
+        // Prepare background - repeated sprite.
+		self.backgroundSprite = [CCSprite spriteWithFile:@"checkerboard.png"]; 
+    }
+    
+    return self;
+}
+
+- (void) dealloc
+{
+    self.backgroundSprite = nil;
+    self.targetNode = nil;
+    
+    [super dealloc];
+}
+
+- (void) updateForScreenReshape
+{
+	CGSize s = [CCDirector sharedDirector].winSize;
+	
+	// Update checkerboard size to fit winSize.
+    [self.backgroundSprite setTextureRect: CGRectMake(0, 0, s.width, s.height)];
+	
+    // Update self size.
+	[self setContentSize: s];
+}
+
+- (void) visit
+{
+    if ([_targetNode isKindOfClass:[CCScene class]])
+    {
+        [_targetNode visit];
+    }
+    else
+    {
+        // Render background.
+        [_backgroundSprite visit];
+        
+        // Render targetNode.
+        [_targetNode visit];
+        
+        // Render borders if needed.        
+        if (self.showBorders)
+        {
+            CGSize s = contentSize_;
+            GLfloat lineWidth = 2.0f;
+            GLfloat halfLineWidth = 0.5f * lineWidth; 
+            
+            // Use Inverted BG Color to Draw the Outline
+            glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+            glLineWidth(lineWidth);
+            
+            CGPoint vertices[] = {
+                ccp(halfLineWidth, s.height - halfLineWidth),
+                ccp(s.width - halfLineWidth, s.height - halfLineWidth),
+                ccp(s.width - halfLineWidth, halfLineWidth),
+                ccp(halfLineWidth, halfLineWidth)
+            };
+            
+            ccDrawPoly(vertices, 4, YES);
+        }
+        
+    }
+}
+
 @end
