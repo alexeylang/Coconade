0546ee64bba52a67d96656448dc3d3f09c29d4f4
Stepan Generalov <psi.pk.ru@gmail.com>
2012-02-12 16:18:32 +0400

    CCNModel: add iVars.

--- Coconade/Classes/CCNModel.h
+++ Coconade/Classes/CCNModel.h
@@ -8,7 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
+@class CCNode;
+
 /** @class CCNModel Main model class. Manages Coconade's project. */
 @interface CCNModel : NSObject
+{
+    /** Holds all nodes of a project with their children. */
+    NSDictionary *_rootNodes;
+    
+    /** Weakref to current selected node from rootNodes. */
+    CCNode *_selectedRootNode;
+    
+    /** SelectedRootNode & All it's Children, sorted by their Z-Order.
+     * Used to search through all nodes in current hierarchy, that is being edited.
+     */
+    NSArray *_currentNodes;
+}
 
 @end
