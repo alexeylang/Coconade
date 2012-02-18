011be0a17d1030359878260fe74c84692727c7d6
Stepan Generalov <psi.pk.ru@gmail.com>
2012-02-18 18:09:02 +0400

    CCNController: Add Import methods to header.

--- Coconade/Classes/CCNController.h
+++ Coconade/Classes/CCNController.h
@@ -29,4 +29,22 @@
  */
 @property(readwrite, retain) CCNModel *model;
 
+#pragma mark Import
+
+/** Returns array of file extensions (excludng dot), that are supported by Coconade
+ * and can be used as images (textures).
+ *
+ * @todo remove method prototype from public interface or private interface in implementation file
+ */
+- (NSArray *) allowedImageFileTypes;
+
+/** Creates CCSprites from given filenames and adds it to current selected node
+ * (curRootNode or selected node).
+ * If sprite can't be added to that node - tries to add it to it's parent.
+ * If it can't be added to anything - registers a problem in CCNProblemManager
+ *
+ * @todo remove method prototype from public interface or private interface in implementation file
+ */
+- (void)importSpritesWithFiles: (NSArray *) filenames;
+
 @end
