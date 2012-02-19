7d8220f75662f4b09b09f33db01f5789f9712cd3
Alexey Lang <alekseylang@gmail.com>
2012-02-19 15:41:56 +0400

    CCNWindow: improve comments.

--- Coconade/Classes/CCNWindow.h
+++ Coconade/Classes/CCNWindow.h
@@ -8,11 +8,15 @@
 #import <AppKit/AppKit.h>
 
 
+/** @class CCNWindow main window for Coconade */
 @interface CCNWindow : NSWindow
 {
     BOOL _needsEnableUpdate;    
 }
 
+/** Disables drawing of window until window’s offscreen buffer flushes to the screen.
+ * This method need to fix flickers of glView on window resize / splitVew resize. 
+ */
 - (void)disableUpdatesUntilFlush;
 
 @end
