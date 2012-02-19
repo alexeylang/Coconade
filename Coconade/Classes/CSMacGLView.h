c7a0e0b6a985c2827b8ad422a5c7899078dcc643
Alexey Lang <alekseylang@gmail.com>
2012-02-19 16:09:50 +0400

    CSMacGLView: refactor to single style.

--- Coconade/Classes/CSMacGLView.h
+++ Coconade/Classes/CSMacGLView.h
@@ -11,26 +11,11 @@
 #import <Cocoa/Cocoa.h>
 #import "cocos2d.h"
 
-@class CSMacGLView;
-@protocol CCNMacGLViewDragAndDropDelegate <NSObject>
-
-@required
-
-/** Called on delegate when glView receives -draggingEntered: message
- * Return value of this method will be returned in CCNMacGLView#draggingEntered:
- */
-- (NSDragOperation)ccnMacGLView: (CSMacGLView *) glView draggingEntered:(id <NSDraggingInfo>)sender;
-
-/** Called on delegate when glView receives -performDragOperation: message
- * Return value of this method will be returned in CCNMacGLView#performDragOperation:
- */
-- (BOOL)ccnMacGLView: (CSMacGLView *) glView performDragOperation:(id <NSDraggingInfo>)sender; 
-
-@end
-
 /** Notification that is sent, when CCNMacGLView instance's workspaceSize is changed. */
 FOUNDATION_EXPORT NSString *const CCNMacGLViewWorkspaceSizeDidChangeNotification;
 
+
+@protocol CCNMacGLViewDragAndDropDelegate;
 @interface CSMacGLView : MacGLView <CCProjectionProtocol>
 {
 	ccDirectorProjection _projection;
@@ -44,12 +29,7 @@ FOUNDATION_EXPORT NSString *const CCNMacGLViewWorkspaceSizeDidChangeNotification
     id <CCNMacGLViewDragAndDropDelegate> _dragAndDropDelegate;
 }
 
-@property (readwrite, assign) id <CCNMacGLViewDragAndDropDelegate> dragAndDropDelegate;
-
-#pragma mark Workspace
-
-/**
- * Size of the viewport, this property sets CCDirector#winSize and
+/** Size of the viewport, this property sets CCDirector#winSize and
  * is used in Cocoshop for setting workspace size
  *
  * Due to NSGLView restrictions and zoom functionalty of the CSMacGLView
@@ -60,18 +40,7 @@ FOUNDATION_EXPORT NSString *const CCNMacGLViewWorkspaceSizeDidChangeNotification
  */
 @property (readwrite) CGSize workspaceSize;
 
-/**
- * Updates window size, to show scrollers of NSScrollView
- */
-- (void) updateWindow;
-
-/* Resizes the View for Centering the Workspace in Window
- * This is needed cause it's impossible to set the position of contentNode of
- * NSScrollView */
-- (void) fixFrameSize;
-
-/**
- * Since CSMacGLView uses custom projection
+/** Since CSMacGLView uses custom projection
  * we need a method, that will allow us to choose between 2D/3D projections
  * without cancelling custom projection.
  * Use this property to set kCCDirectorProjection3D || kCCDirectorProjection2D
@@ -81,30 +50,54 @@ FOUNDATION_EXPORT NSString *const CCNMacGLViewWorkspaceSizeDidChangeNotification
  */
 @property (readwrite) ccDirectorProjection projection;
 
-#pragma mark Zoom
-
-/**
- * Zoom factor just like in GIMP or other graphics editors
+/** Zoom factor just like in GIMP or other graphics editors
  * Zooms the node with changing glViewport
  * 1.0f is for 100% Scale
  */
 @property (readwrite) CGFloat zoomFactor;
-/**
- * How fast to zoom (default is 0.1f)
+
+/** How fast to zoom (default is 0.1f)
  */
 @property (readwrite) CGFloat zoomSpeed;
-/**
- * Maximum zoom (default is 3.0f)
+
+/** Maximum zoom (default is 3.0f)
  */
 @property (readwrite) CGFloat zoomFactorMax;
-/**
- * Minimum zoom (default is 0.1f)
+
+/** Minimum zoom (default is 0.1f)
  */
 @property (readwrite) CGFloat zoomFactorMin;
 
-/**
- * Reset the zoom to default
+@property (readwrite, assign) id <CCNMacGLViewDragAndDropDelegate> dragAndDropDelegate;
+
+/** Updates window size, to show scrollers of NSScrollView
+ */
+- (void) updateWindow;
+
+/* Resizes the View for Centering the Workspace in Window
+ * This is needed cause it's impossible to set the position of contentNode of
+ * NSScrollView */
+- (void) fixFrameSize;
+
+/** Reset the zoom to default
  */
 - (void)resetZoom;
+
+@end
+
+
+@protocol CCNMacGLViewDragAndDropDelegate <NSObject>
+
+@required
+
+/** Called on delegate when glView receives -draggingEntered: message
+ * Return value of this method will be returned in CCNMacGLView#draggingEntered:
+ */
+
+- (NSDragOperation)ccnMacGLView: (CSMacGLView *) glView draggingEntered:(id <NSDraggingInfo>)sender;
+/** Called on delegate when glView receives -performDragOperation: message
+ * Return value of this method will be returned in CCNMacGLView#performDragOperation:
+ */
+- (BOOL)ccnMacGLView: (CSMacGLView *) glView performDragOperation:(id <NSDraggingInfo>)sender; 
 
 @end
