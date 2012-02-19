6a5e37dbe657d059360053a39f037afaec23c2d7
Alexey Lang <alekseylang@gmail.com>
2012-02-19 15:21:13 +0400

    CCNWindowController: imrove comments.

--- Coconade/Classes/CCNWindowController.h
+++ Coconade/Classes/CCNWindowController.h
@@ -7,15 +7,22 @@
 
 #import <AppKit/AppKit.h>
 
-// @TODO: add comments to ALL
+
 @class CSMacGLView;
+/** @class CCNWindowController controller for Coconade window and its subviews. 
+ * Contains all logic of creating, setup and work menus, toolbars, views, etc.
+ */
 @interface CCNWindowController : NSWindowController <NSToolbarDelegate, NSSplitViewDelegate>
 {
 	CSMacGLView	*_glView;
 }
 
+/** Holds OpenGL view that used for representation all Cocos2D objects. */
 @property (readwrite, retain) CSMacGLView *glView;
 
+/** Prepare Coconade window - creates and sets up main menu, toolbar, glView, 
+ * splitView, scrollView, etc.
+ */
 - (void) prepareWindow;
 
 @end
