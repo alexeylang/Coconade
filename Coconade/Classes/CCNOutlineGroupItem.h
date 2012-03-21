1fbc1d92d03db8a568c2842cd599d0eb4e126c05
Alexey Lang <alekseylang@gmail.com>
2012-03-21 18:42:49 +0400

    CCNOutlineViewGroupItem: improve comments.

--- Coconade/Classes/CCNOutlineGroupItem.h
+++ Coconade/Classes/CCNOutlineGroupItem.h
@@ -9,20 +9,32 @@
 #import <Foundation/Foundation.h>
 
 
+/** @class CCNOutlineGroupItem item in left outline view that represents root group
+ * like "Nodes", "Textures" etc.
+ */
 @interface CCNOutlineGroupItem : NSObject
 {
     NSMutableArray *_children;
     NSString *_name;
 }
 
+/** Holds array with current child items. */
 @property (readwrite, retain) NSMutableArray *children;
 
+/** Holds current name string. */
 @property (readwrite, copy) NSString *name;
 
 #pragma mark Init/Create
 
+/** Returns new autoreleased item, inited with given name. 
+ * @see -initWithName:
+ */
 + (id)itemWithName:(NSString *)name;
 
+/** Designated init for CCNOutlineGroupItem.
+ *
+ * @param name NSString with name for item.
+ */
 - (id)initWithName:(NSString *)name;
 
 @end
